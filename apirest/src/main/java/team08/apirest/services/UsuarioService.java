e024420538b587026193867b48927e4f8794f1cb
Alan2Romero <filliad123@gmail.com>
2026-07-29 21:09:40 -0300

    Implementacion de la comunicacion API Rest con API Python

--- apirest/src/main/java/team08/apirest/services/UsuarioService.java
+++ apirest/src/main/java/team08/apirest/services/UsuarioService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import team08.apirest.models.UsuarioModel;
 import team08.apirest.repositories.UsuarioRepository;
@@ -14,14 +15,33 @@ public class UsuarioService{
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    // URL Script de Python
+    private final String PYTHON_SERVICE_URL = "http://localhost:5000/calcular-finanzas";
+
     // LISTA COMPLETA DE USUARIOS
     public ArrayList<UsuarioModel> obtenerUsuarios(){
         return (ArrayList<UsuarioModel>) usuarioRepository.findAll();
     }
     // AGREGAR UN NUEVO USUARIO
     public UsuarioModel guardarUsuario(UsuarioModel usuario){
+        RestTemplate restTemplate = new RestTemplate();
+        
+        try {
+            // ENVIO AL USUARIO 
+            UsuarioModel usuarioCalculado = restTemplate.postForObject(PYTHON_SERVICE_URL, usuario, UsuarioModel.class);
+            
+            // SI ES EXITOSO, SE GUARDAN LOS DATOS
+            if (usuarioCalculado != null) {
+                return usuarioRepository.save(usuarioCalculado);
+            }
+        } catch (Exception e) {
+            System.err.println("Error al comunicarse con el modelo de Python: " + e.getMessage());
+        }
+        
+        // Plan de respaldo: Si Python falla, guarda el usuario tal cual llego para que la app no falle
         return usuarioRepository.save(usuario);
     }
+    
     // ELIMINAR UN USUARIO POR ID
     public boolean eliminarUsuario(Long id){
         try{
