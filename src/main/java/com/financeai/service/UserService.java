32e42a11bea01efbbe029294e952046fbf5591d1
Leandro74377 <leandrobaque4@gmail.com>
2026-08-20 16:07:16 -0500

    Actualizar UserService.java

--- src/main/java/com/financeai/service/UserService.java
+++ src/main/java/com/financeai/service/UserService.java
@@ -1,13 +1,13 @@
 package com.financeai.service;
 
-import com.financeai.entity.Usuario;
+import com.financeai.entity.User;
 import java.util.Optional;
 
 public interface UserService {
-    Usuario createUser(String email, String password, String firstName, String lastName);
-    Optional<Usuario> getUserById(Long id);
-    Optional<Usuario> getUserByEmail(String email);
-    Usuario updateUser(Long id, Usuario user);
+    User createUser(String email, String password, String firstName, String lastName);
+    Optional<User> getUserById(Long id);
+    Optional<User> getUserByEmail(String email);
+    User updateUser(Long id, User user);
     void deleteUser(Long id);
-    Usuario updateFinancialData(Long userId, Double income, Double expenses, Double emergencyFund, Double debt);
+    User updateFinancialData(Long userId, Double income, Double expenses, Double emergencyFund, Double debt);
 }
