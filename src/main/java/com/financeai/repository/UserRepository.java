c890c59b5a1fd0c0bdc67c3b0399dc7c9b3fe05d
Leandro74377 <leandrobaque4@gmail.com>
2026-08-20 13:52:30 -0500

    Actualizar UserRepository.java

--- src/main/java/com/financeai/repository/UserRepository.java
+++ src/main/java/com/financeai/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.financeai.repository;
 
-import com.financeai.entity.Usuario;
+import com.financeai.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
