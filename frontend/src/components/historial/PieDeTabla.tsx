fb34cfab37985ce9a82a24186161c0e3194af3ed
fnandoth <fernandoarielthielelepe@gmail.com>
2026-08-22 12:48:22 -0400

    importacion tipos

--- frontend/src/components/historial/PieDeTabla.tsx
+++ frontend/src/components/historial/PieDeTabla.tsx
@@ -1,5 +1,5 @@
 import { Download} from "lucide-react";
-import { PieDeTablaProps } from "../types/transaction-type";
+import { PieDeTablaProps } from "../../types/transaction-type";
 import "./PieDeTabla.css";
 
 export default function PieDeTabla({
