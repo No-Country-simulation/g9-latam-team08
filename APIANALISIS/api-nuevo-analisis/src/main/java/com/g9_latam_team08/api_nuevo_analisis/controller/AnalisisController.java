1a87c8f695ea65fe706453c979f072a9b9884f6b
Juan Manuel Roldan <juanroldancaicedo@gmail.com>
2026-08-22 22:54:41 -0300

    corrigiendo endpoint del controller

--- APIANALISIS/api-nuevo-analisis/src/main/java/com/g9_latam_team08/api_nuevo_analisis/controller/AnalisisController.java
+++ APIANALISIS/api-nuevo-analisis/src/main/java/com/g9_latam_team08/api_nuevo_analisis/controller/AnalisisController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/nuevo-analisis/procesar-perfil")
+@RequestMapping("/api/usuarios/analisis")
 @CrossOrigin(origins = "*")
 public class AnalisisController {
 
